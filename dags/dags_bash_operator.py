9d81dda5dad0d50eed67256ea596b4ccece97130
doosung3672 <doosungkang@gmail.com>
2024-01-30 00:26:13 +0900

    오류수정

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -16,8 +16,7 @@ with DAG(
     )
     bash_t2 = BashOperator(
         task_id="bash_t2",
-        bas
-        h_command="echo $HOSTNAME",
+        bash_command="echo $HOSTNAME",
     )
 
     bash_t1 >> bash_t2
