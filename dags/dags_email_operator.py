496416b743156f48669ed471859c0ab50b6290dc
doosung3672 <doosungkang@gmail.com>
2024-02-01 16:54:08 +0900

    email operator 업로드7

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -9,7 +9,7 @@ with DAG(
     start_date=pendulum.datetime(2024, 1, 29, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
-    send_email_task = BashOperator(
+    send_email_task = EmailOperator(
       task_id='send_email_task',
       to = 'doosungkang@naver.com',
       subject ='airflow 성공메일',
