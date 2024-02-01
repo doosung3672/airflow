90b96e286df776a5158671a052966e22ff78ef12
doosung3672 <doosungkang@gmail.com>
2024-02-01 15:21:06 +0900

    email operator 업로드

---
+++ dags/dags_email_operator.py
@@ -0,0 +1,22 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from airflow.operators.email import EmailOperator
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2024, 1, 29, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    send_email_task = EmailOperator{
+        task_id = 'send_email_task'
+        to = 
+    }
+    send_email_task = BashOperator(
+      task_id='send_email_task'",
+      to = 'doosungkang@naver.com',
+      cc = 'doosungkang@hanmail.net',
+      subject ='airflow 성공메일',
+      html_content = 'airflow 작업이 완료되었습니다.'
+    )    
