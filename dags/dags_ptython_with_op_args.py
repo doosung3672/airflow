ee17da8b474158eeea054dc3a03996cd53a158fe
doosung3672 <doosungkang@gmail.com>
2024-02-03 23:24:15 +0900

    python op

---
+++ dags/dags_ptython_with_op_args.py
@@ -0,0 +1,21 @@
+from airflow.models.dag import DAG
+import datetime
+import pendulum
+from airflow.operators.python  import PythonOperator
+from common.common_func  import regist
+
+with DAG(
+    dag_id="dags_bash_select_fruit",
+    schedule="10 0 * * 6#1",
+    start_date=pendulum.datetime(2024, 1, 31, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    regist_t1 = PythonOperator (
+       task_id="regist_t1",
+       python_callable=regist,
+       op_args=['doosung','man','kr','incheon']
+    )
+    
+
+    regist_t1
