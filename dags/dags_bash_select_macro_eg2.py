ffc5d7134530b37cdf560d495172ae3575ecb6d7
doosung3672 <doosungkang@gmail.com>
2024-02-05 21:53:08 +0900

    얼로드

---
+++ dags/dags_bash_select_macro_eg2.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+import datetime
+import pendulum
+from airflow.operators.bash import BashOperator
+from dateutil import relativedelta
+
+with DAG(
+    dag_id="dags_bash_select_macro_eg2",
+    schedule="10 0 * * 6#2",
+    start_date=pendulum.datetime(2024, 2, 5, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    # START_DATE: 전월 말일, END_DATE:1일 전
+    bash_task2 = BashOperator(
+        task_id = 'bash_task2',
+        env={'START_DATE':'{{(data_interval_end.intimezone("Asia/Seoul") - macros.deteutil.relativedelta.relativedelta(days=19)) | ds }}',
+             'END_DATE':'{{(data_interval_end.intimezone("Asia/Seoul") - macros.deteutil.relativedelta.relativedelta(days=14)) | ds }}'
+        },
+        bash_command='echo "START_DATE: $START_DATE && echo "END_DATE: $END_DATE"'
+    )
