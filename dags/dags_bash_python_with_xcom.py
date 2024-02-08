fee04ff13dcdf5c5612083025d4a832774290856
doosung3672 <doosungkang@gmail.com>
2024-02-08 17:42:00 +0900

    '업로드3'

--- dags/dags_bash_python_with_xcom.py
+++ dags/dags_bash_python_with_xcom.py
@@ -19,13 +19,28 @@ with DAG(
     bash_pull = BashOperator(
         task_id = 'bash_pull',
         env={
-            'STATUS':'{{ ti.xcm_pull(task_ids="python_push")["status"] }}',
-            'DATA':'{{ ti.xcm_pull(task_ids="python_push")["data"] }}',
-            'OPTIONS_CNT':'{{ti.xcm_pull(task_ids="python_push")["options_cnt"] }}'
+            'STATUS':'{{ ti.xcom_pull(task_ids="python_push")["status"] }}',
+            'DATA':'{{ ti.xcom_pull(task_ids="python_push")["data"] }}',
+            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["options_cnt"] }}'
         },
         bash_command='echo $STATUS &&echo $DATA && echo $OPTIONS_CNT'
     )
     
     pythonb_push_xcom() >> bash_pull
 
-    
+    bash_push = BashOperator(
+        task_id ='bash_push',
+        bash_command='echo PUSH_START '
+        '{{ti.xcom_push(key="bash_pushed",value=200) }} && '
+        'echo PUSH_COMPLETE'
+    )
+
+    @task(task_id ='python_pull')
+    def python_pull_xcom(**kwargs):
+        ti = kwargs['ti']
+        status_value=ti.xcom_pull(key='bash_pushed')
+        return_value=ti.xcom_pull(task_ids='bash_push')
+        print('status_value'+str(status_value))
+        print('return_value'+str(return_value))
+
+    bash_push >> python_pull_xcom()
