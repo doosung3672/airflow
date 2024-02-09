c3558ab3ff4fbb80c8333493fa70ec58ff6317b1
doosung3672 <doosungkang@gmail.com>
2024-02-09 22:19:45 +0900

    'branch24'

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -9,7 +9,7 @@ from airflow.operators.python import BranchPythonOperator
 with DAG(
     dag_id="dags_branch_python_operator",
     schedule="0 0 * * *",
-    start_date=pendulum.datetime(2024, 2, 10, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2024, 2, 9, tz="Asia/Seoul"),
     catchup=False,
 ) as dag:
     def select_random():
