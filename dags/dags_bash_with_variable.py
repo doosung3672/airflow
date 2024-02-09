082542a5496ca58a7d096ed74617628e6f8e2131
doosung3672 <doosungkang@gmail.com>
2024-02-09 21:27:37 +0900

    'varibable139'

--- dags/dags_bash_with_variable.py
+++ dags/dags_bash_with_variable.py
@@ -12,13 +12,13 @@ with DAG(
 ) as dag:
     var_value = Variable.get("sample_key")
 
-    bash_t1 = BashOperator(
-        task_id="bash_t1",
-        bash_command="echo whoami",
+    bash_var_1 = BashOperator(
+        task_id="bash_var_1",
+        bash_command="echo variable:{var_value}",
     )
     bash_t2 = BashOperator(
         task_id="bash_t2",
         bash_command="echo $HOSTNAME",
     )
 
-    bash_t1 >> bash_t2
+    bash_var_1 >> bash_t2
