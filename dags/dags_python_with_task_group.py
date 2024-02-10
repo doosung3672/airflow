2a75144104e9f9ea0699ffd77496cf4b39480584
doosung3672 <doosungkang@gmail.com>
2024-02-10 22:36:05 +0900

    'group108'

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -17,13 +17,11 @@ with DAG(
     def inner_func(**kwargs):
         msg = kwargs.get('msg')  or ''
         print(msg)
-    
 
-
-        with TaskGroup(group_id='second_group',tooltip='두번째 그룹입니다') as group_2:
-            ''' 여기에 적은 docstring은 표시되지 않습니다. '''
-            @task(task_id='inner_function1')
-            def inner_func1(**kwargs):
+    with TaskGroup(group_id='second_group',tooltip='두번째 그룹입니다') as group_2:
+         ''' 여기에 적은 docstring은 표시되지 않습니다. '''
+         @task(task_id='inner_function1')
+         def inner_func1(**kwargs):
                 print('두번째 TaskGroup 내 첫번째 task입니다.')
 
 
