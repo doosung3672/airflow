1efd6cdad36d32b20ed2ed3732d6cb761ee41de3
doosung3672 <doosungkang@gmail.com>
2024-02-10 22:03:27 +0900

    'group5'

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -34,6 +34,17 @@ with DAG(
 
         inner_function1() >> inner_function2
 
- 
-       # group_1() >> group_2
-    group_1()
+        with TaskGroup(group_id='second_group',tooltip='두번째 그룹입니다.') as group_2:
+            ''' 여기에 적은 docstring은 표시되지 않습니다. '''
+            @task(task_id=inner_function1)
+            def inner_func1(**kwargs):
+                print('두번째 TaskGroup 내 첫번째 task입니다.')
+            inner_function2 = PythonOperator(
+                task_id='inner_function2',
+                python_callable=inner_func,
+                op_kwargs={'msg':'두번째 TaskGroup내 두번째 task입니다.'}
+            )                
+            inner_func1() >> inner_function2
+            
+
+        group_1() >> group_2
