4709a7d1b176c986b2a41de91533449bb0dc5e4f
doosung3672 <doosungkang@gmail.com>
2024-02-10 19:00:28 +0900

    'branckoperator

---
+++ dags/dags_base_branch_operator.py
@@ -0,0 +1,50 @@
+from airflow.models.dag import DAG
+import datetime
+from airflow.utils.context import Context
+import pendulum
+import random
+from airflow.operators.python import PythonOperator
+from airflow.operators.branch import BaseBranchOperator
+
+
+with DAG(
+    dag_id="dags_base_branch_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2024, 2, 9, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    class CustomBranchOperator(BaseBranchOperator):
+        def choose_branch(self, context: Context):
+            import random
+            print(Context)
+            item_lst = ['A','B','C']
+            selected_item = random.choice(item_lst)
+            if selected_item == 'A':
+                return 'task_a'
+            elif selected_item in ['B','C']:
+                return ['task_b','task_c']
+    custom_branch_operator = CustomBranchOperator(task_id='python_branch_task')
+        
+    def common_func(**kwargs):
+        print(kwargs['selected'])
+    
+    task_a = PythonOperator(
+        task_id ='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
+        
+    task_b = PythonOperator(
+        task_id ='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )
+
+    task_c = PythonOperator(
+        task_id ='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+
+    custom_branch_operator >> [task_a,task_b,task_c]
+
